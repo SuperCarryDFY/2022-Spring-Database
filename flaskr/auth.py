c7692f39277f109fefb166e9777076e29a108997
Dai <1003566731@qq.com>
2022-05-14 21:04:21 +0800

    enable register

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -14,20 +14,25 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 def register():
     if request.method == 'POST':
 
-        username = request.form['username']
+        account = request.form['account']
         password = request.form['password']
+        type = request.form['type']
         db = get_db()
         error = None
 
-        if not username:
-            error = 'Username is required.'
+        if not account:
+            error = 'account is required.'
         elif not password:
             error = 'Password is required.'
+        elif not type:
+            error = 'type is required.'
+
+        print('type',type)
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO car_sys.user (username, password) VALUES ('{}', '{}')".format(username,generate_password_hash(password))
+                    "INSERT INTO car_sys.login (account, password,type) VALUES ('{}', '{}','{}')".format(account,generate_password_hash(password),type)
                 )
                 # db.commit()
             
