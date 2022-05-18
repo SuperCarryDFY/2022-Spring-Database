11de7fd737f4f40ff76ee7206ad1bef71800e33f
Dai <1003566731@qq.com>
2022-05-18 13:27:32 +0800

    add function getnumber() and add url /client/Gnumber

--- flaskr/client.py
+++ flaskr/client.py
@@ -9,6 +9,23 @@ from flaskr.db import get_db
 
 bp = Blueprint('client', __name__, url_prefix='/client')
 
+def getnumber():
+    db = get_db()
+    row = db.prepare("select * from car_sys.client;")
+    last_record = row()[-1]
+    # last_record = "('GS0030', '上海戬臻实业有限公司', '个人', 100, '王', '13858646391')"
+    client_number = int(last_record[0][2:])
+    # client_number = 30
+    client_number += 1
+    client_number_str = str(client_number)
+    client_number_str = (4-len(client_number_str))*'0' + client_number_str
+    return 'GS'+ client_number_str
+
+
+@bp.route('/Gnumber', methods=["GET"])
+def Gnumber():
+    return getnumber()
+
 
 @bp.route('/register', methods=('POST','GET'))
 def register():
@@ -16,16 +33,12 @@ def register():
         db = get_db()
         error = None
         # 获取表单数据
+        client_number = getnumber()
         client_name = request.form['name']
         client_cha = request.form['type']
         discount = request.form['discount']
         contact_man = request.form['contact_man']
         contact_number = request.form['contact_number']
-        # 生成客户编号
-        num = db.prepare("select count(*) from car_sys.client")
-        num = str(num()[0][0])
-        num = (4-len(num))*'0'+num
-        client_number = 'GS'+num
         try:
             db.execute(
                 "INSERT INTO car_sys.client (client_number, client_name, client_cha, discount, contact_man, contact_number) VALUES('{}','{}','{}','{}','{}','{}')".format(
